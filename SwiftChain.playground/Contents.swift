5aef578bc6b71d99415176bf20e5d43bb864f47c
gg2001 <gautham.gg@gmail.com>
2018-04-01 09:38:12 +1000

    fixes

--- SwiftChain.playground/Contents.swift
+++ SwiftChain.playground/Contents.swift
@@ -2,9 +2,23 @@ import UIKit
 import PlaygroundSupport
 
 class MyViewController : UIViewController {
-    let label = UILabel()
+    public func random(_ range:Range<Int>) -> Int
+    {
+        return range.lowerBound + Int(arc4random_uniform(UInt32(range.upperBound - range.lowerBound)))
+    }
+    
+    var account1 = UILabel()
+    var balance1 = UILabel()
+    var account2 = UILabel()
     
     override func loadView() {
+        let genesisKey = random(100..<999)
+        let genesisAddress = genesisKey * 5
+        var testKey = random(100..<999)
+        let testAddress = testKey * 5
+        var accounts: [String: Int] = ["0000": 21000000]
+        let reward = 50
+        
         class Block {
             var hash = String()
             var data = String()
@@ -36,31 +50,76 @@ class MyViewController : UIViewController {
         }
         
         let swiftchain = BlockChain()
-        swiftchain.createGenesisBlock(data: "From: Gautham; To: Apple; Amount: 10XSC")
-        swiftchain.addBlock(data: "From: Apple; To: Gautham; Amount: 8XSC")
-        swiftchain.addBlock(data: "From: Gautham; To: Apple; Amount: 2XSC")
-        // swiftchain.chain[1].data <- prints data
-        // swiftchain.chain[1].hash = "invalid" <- renders chain invalid
-        
-        for i in 0...swiftchain.chain.count-1 {
-            print("\tBlock: \(swiftchain.chain[i].index)\n\tHash: \(swiftchain.chain[i].hash)\n\tPreviousHash: \(swiftchain.chain[i].previousHash)\n\tData: \(swiftchain.chain[i].data)\n")
-        }
-        
-        var isChainValid = true
-        for i in 1...swiftchain.chain.count-1 {
-            if swiftchain.chain[i].previousHash != swiftchain.chain[i-1].hash {
-                isChainValid = false
+        func transaction(from: String, to: String, amount: Int, type: String) {
+            if accounts[from] == nil {
+                exit(0)
+            } else if accounts[from]!-amount < 0 {
+                exit(0)
+            } else {
+                accounts.updateValue(accounts[from]!-amount, forKey: from)
+            }
+            if accounts[to] == nil {
+                accounts.updateValue(amount, forKey: to)
+            } else {
+                accounts.updateValue(accounts[to]!+amount, forKey: to)
+            }
+            if type == "genesis" {
+                swiftchain.createGenesisBlock(data: "From: \(from); To: \(to); Amount: \(amount)XSC")
+            } else if type == "normal" {
+                swiftchain.addBlock(data: "From: \(from); To: \(to); Amount: \(amount)XSC")
+            }
+            if amount < 0 {
+                exit(0)
             }
         }
-        print("Chain is valid: \(isChainValid)")
+        transaction(from: "0000", to: "\(genesisAddress)", amount: 50, type: "genesis")
+        transaction(from: "\(genesisAddress)", to: "\(testAddress)", amount: 10, type: "normal")
+        
+        func chainValidity() -> String {
+            var isChainValid = true
+            for i in 1...swiftchain.chain.count-1 {
+                if swiftchain.chain[i].previousHash != swiftchain.chain[i-1].hash {
+                    isChainValid = false
+                }
+            }
+            return "Chain is valid: \(isChainValid)\n"
+        }
+        func chainState() {
+            for i in 0...swiftchain.chain.count-1 {
+                print("\tBlock: \(swiftchain.chain[i].index)\n\tHash: \(swiftchain.chain[i].hash)\n\tPreviousHash: \(swiftchain.chain[i].previousHash)\n\tData: \(swiftchain.chain[i].data)")
+            }
+            print(accounts)
+            print(chainValidity())
+        }
+        chainState()
         
         let view = UIView()
         view.backgroundColor = .white
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "SwiftChain"
-        label.textColor = .black
-        view.addSubview(label)
+        
+        //label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        account1.text = "Account: \(genesisAddress)"
+        account1.textColor = .black
+        account1.translatesAutoresizingMaskIntoConstraints = false
+        balance1.text = "Balance: "
+        balance1.textColor = .black
+        balance1.translatesAutoresizingMaskIntoConstraints = false
+        
+        let margins = view.layoutMarginsGuide
+        /*
+        NSLayoutConstraint.activate([
+            account1.leadingAnchor.constraint(equalTo: balance1.leadingAnchor),
+            account1.topAnchor.constraint(equalTo: balance1.bottomAnchor, constant: 10),
+            ])
+        */
+        
+        view.addSubview(account1)
+        view.addSubview(balance1)
         self.view = view
+        
+        transaction(from: "\(testAddress)", to: "5789", amount: 10, type: "normal")
+        chainState()
+        transaction(from: "5789", to: "\(testAddress)", amount: 10, type: "normal")
+        chainState()
     }
 }
 
